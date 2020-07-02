1bde7e81ddb3c4981cd6d8c745978d3cb775b4a6
Digital Sleuth <62841822+digitalsleuth@users.noreply.github.com>
2020-07-02 02:22:20 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf8') as readme:
+    long_description = readme.read()
+
+setup(
+    name="time_decode",
+    version="2.2",
+    author="Corey Forman",
+    license="MIT",
+    url="https://github.com/digitalsleuth/time_decode",
+    description=("Python 3 timestamp decode/encode tool"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "python-dateutil",
+        "colorama"
+    ],
+    package_data={'': ['README.md, LICENSE.md']}
+)
