a611aaed9636ac0770a69e06f6636bbe86c8af48
digitalsleuth <corey@digitalsleuth.ca>
2023-12-24 02:57:29 +0000

    v7 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding='utf8') as readme:
 
 setup(
     name="time_decode",
-    version="6.1.0",
+    version="7.0.0",
     author="Corey Forman",
     license="MIT",
     url="https://github.com/digitalsleuth/time_decode",
@@ -20,6 +20,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
+        "PyQt6",
         "python-dateutil",
         "colorama"
     ],
